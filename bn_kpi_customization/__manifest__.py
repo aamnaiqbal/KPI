946c25bf368f5eb7cce12e42f084701c438de174
Aamna Iqbal <bytesnode@gmail.com>
2026-08-08 19:29:06 +0500

    menu updated

--- bn_kpi_customization/__manifest__.py
+++ bn_kpi_customization/__manifest__.py
@@ -9,8 +9,8 @@
     'depends': ['base', 'crm', 'sale_management', 'contacts', 'account', 'hr_attendance', 'eh_board'],
     'data': [
         'security/ir.model.access.csv',
-        'views/dashboard_menu.xml',
         'views/kpi_view.xml',
+        'views/dashboard_menu.xml',
         'views/parameter_master_setup.xml',
         'views/employee_target.xml',
     ],
